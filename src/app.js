f9883230c144d1ae29a2ac25b9facda879ca18ad
Maxwel Hernandez <maxwelhernandez@gmail.com>
2026-03-28 18:29:24 +0000

    creando variables y funcion ramdom

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ['mine', 'yours', 'his', 'hers', 'ours', 'theirs'];
+let adjetive = ['awesone', 'large', 'fast', 'big', 'huge'];
+let noun = ['jogger', 'raccon', 'city', 'table'];
+let extensions = ['.com', '.net', '.us', '.ve'];
+
+function generateRandomDomain() {
+  let promombre = pronoun[Math.floor(Math.random() * pronoun.length)];
+}
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+

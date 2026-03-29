4ef2ebb9059238a548cb808ac48194053172a02c
Maxwel Hernandez <maxwelhernandez@gmail.com>
2026-03-29 01:31:41 +0000

    funciones generadora

--- src/app.js
+++ src/app.js
@@ -5,18 +5,48 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let pronoun = ['mine', 'yours', 'his', 'hers', 'ours', 'theirs'];
+let pronoun = ['The', 'yours', 'ours','theirs'];
 let adjetive = ['awesone', 'large', 'fast', 'big', 'huge'];
-let noun = ['jogger', 'raccon', 'city', 'table'];
+let noun = ['freedom', 'river', 'city', 'table'];
 let extensions = ['.com', '.net', '.us', '.ve'];
 
+// Funcion que permite Generar dominios aleatorios
+
 function generateRandomDomain() {
-  let promombre = pronoun[Math.floor(Math.random() * pronoun.length)];
+  let pronombre = pronoun[Math.floor(Math.random() * pronoun.length)];
+  let adjetivo = adjetive[Math.floor(Math.random() * adjetive.length)];
+  let sustantivo = noun[Math.floor(Math.random() * noun.length)];
+  let extensiones = extensions[Math.floor(Math.random() * extensions.length)];
+
+  return `${pronombre}${adjetivo}${sustantivo}${extensiones} `;
+
 }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// Funcion que permite generar todos los dominios posibles
 
+function generateAllDomains() {
+  let domains = [];
+  for (let pronombre of pronoun) {
+    for (let adjetivo of adjetive) {
+      for (let sustantivo of noun) {
+        for (let extensiones of extensions) {
+          domains.push(`${pronombre}${adjetivo}${sustantivo}${extensiones}`);
+        }
+      }
+    }
+  }
 
+  return domains;
+}
+
+// Mostrar un dominio al cargar
+let domainElement = document.getElementById('domain');
+domainElement.textContent = generateRandomDomain();
+
+// Escuchar clic en el botón
+let btn = document.getElementById('domain_btn');
+btn.addEventListener('click', () => {
+  let newDomain = generateRandomDomain();
+  domainElement.textContent = newDomain;
+
+});
